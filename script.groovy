9bd10b3dbcd92d096e6947d5154277abc38aff93
Gbadamosi Tolulope <tolux17@gmail.com>
2022-07-27 23:16:15 -0500

    Incremental Version

--- script.groovy
+++ script.groovy
@@ -13,9 +13,9 @@ def BuildImage(){
     script {
             echo "Building the Docker image"
             withCredentials([usernamePassword(credentialsId: "Dockerhub", usernameVariable: "user", passwordVariable: "pass")]){
-                sh "docker build . -t  tolux17tech/$IMAGENAME"
+                sh "docker build . -t  tolux17tech//demo:$IMAGENAME"
                 sh "echo $pass | docker login -u $user --password-stdin"
-                sh "docker push tolux17tech/$IMAGENAME"
+                sh "docker push tolux17tech/demo:$IMAGENAME"
                     }
                 }
     
